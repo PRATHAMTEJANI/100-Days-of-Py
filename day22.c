5cba831d23f01164649d86bf8e12dc63b30ca18e
PRATHAMTEJANI <prathamtejani4@gmail.com>
2026-02-21 03:52:16 +0530

    Add files via upload

---
+++ day22.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+
+
+int value(char c){
+    switch (c){
+        case 'I' : return 1;
+        case 'V' : return 5;    
+        case 'X' : return 10;
+        case 'L' : return 50;
+        case 'C' : return 100;
+        case 'D' : return 500;
+        case 'M' : return 1000;
+        default : return 0;
+    }
+}
+
+int romanToint(char* s){
+    int r = 0;
+    for(int i = 0;s[i] != '\0' ; i++){
+        int curr = value(s[i]);
+        int next = value(s[i+1]);
+
+        if(curr < next){
+            r -= curr;
+        }
+        else{
+            r += curr;
+        }
+    }
+    return r;
+}
+int main()
+{
+    char roman[] = "MCMXCIV";
+    printf("%d" , romanToint(roman));
+    
+    
+}
