8ded8c5b979c859e33f98389909e5dc3113a7244
PRATHAM TEJANI <prathamtejani4@gmail.com>
2026-05-08 11:53:18 +0530

    47

---
+++ day47.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+    int nums[] = {2, 3, 1, 1, 4};
+    int n = sizeof(nums) / sizeof(nums[0]);
+
+    int index = nums[0];
+
+    for (int i = 0; i < n; i++)
+    {
+        
+    }
+}
